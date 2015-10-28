9b72635c84e9de900597f31c9ca079a86e18bf1f
Isabella Organ <isabellaorgan@gmail.com>
2015-10-28 11:23:30 -0700

    add router and request handler, continue with exercises

---
+++ router.js
@@ -0,0 +1,5 @@
+function route(pathname) {
+	console.log("About to route a rquest for " + pathname);
+}
+
+exports.route = route;
--- server.js
+++ server.js
@@ -1,16 +1,23 @@
 var http = require("http");
+var url = require("url");
 //refactor:
-function onRequest(request, response) {
-	response.writeHead(200, {"Content-Type": "text/plain"});
-	response.write("Hello World");
-	response.end();
+function start(route) {
+	function onRequest(request, response) {
+		var pathname = url.parse(request.url).pathname;
+		console.log("Request for " + pathname + " received");
+
+		route(pathname);
+
+		response.writeHead(200, {"Content-Type": "text/plain"});
+		response.write("Hello World");
+		response.end();
+	}
+
+	http.createServer(onRequest).listen(8888);
+	console.log("Server has started.");
 }
 
-http.createServer(onRequest).listen(8888);
-
-console.log("Server has started.");
-
-
+exports.start = start;
 
 // http.createServer(function(request, response) {
 // 	response.writeHead(200, {"Content-Type": "text/plain"});
